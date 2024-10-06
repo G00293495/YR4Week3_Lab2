2cf75c9caf8fb18c433dd798b94b6741e6fa0ce7
Paddy Downey <G00392495@atu.ie>
2024-10-06 14:23:12 +0100

    2nd application created

---
+++ src/main/java/productapp/week3_programme2/Week3Programme2Application.java
@@ -0,0 +1,13 @@
+package productapp.week3_programme2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3Programme2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3Programme2Application.class, args);
+    }
+
+}
---
+++ src/test/java/productapp/week3_programme2/Week3Programme2ApplicationTests.java
@@ -0,0 +1,13 @@
+package productapp.week3_programme2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3Programme2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
