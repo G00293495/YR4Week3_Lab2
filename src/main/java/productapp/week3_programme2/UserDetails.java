6d6e619719a6cf7226a17435442786a026d5c42c
Paddy Downey <G00392495@atu.ie>
2024-10-06 15:10:57 +0100

    application built

---
+++ src/main/java/productapp/week3_programme2/UserDetails.java
@@ -0,0 +1,15 @@
+package productapp.week3_programme2;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetails {
+    public String name;
+
+    public String email;
+}
